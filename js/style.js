b04097b9588aba3292c643a455d116d8c51bee1e
sayed42000 <you@example.com>
2019-10-14 20:20:53 +0600

    first commit

---
+++ js/style.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        infinite: true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
